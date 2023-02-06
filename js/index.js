e8e68d88bd336418c2c50da4b296b2fcd6e3280a
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-06 12:06:46 -0500

    termine parte de mobile

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 const hamburgerIcon = document.querySelector('.nav_hamburger');
 const navOverlay = document.querySelector('.nav_overlay');
+const textarea = document.querySelector('textarea')
 
 hamburgerIcon.addEventListener('click', () => {
   hamburgerIcon.classList.toggle('nav_hamburger--open');
 
   navOverlay.classList.toggle('nav_overlay--show');
+})
+
+textarea.addEventListener("keyup", e => {
+  textarea.style.height = '300px';
+  let scHeight = e.target.scrollHeight;
+  textarea.style.height = `${scHeight}px`
 })
