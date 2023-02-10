9174f04e9ab3a1386eefe4d1cd85f97d5195642c
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-09 19:54:56 -0500

    arregle bugs

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,18 @@
 const hamburgerIcon = document.querySelector('.nav_hamburger');
 const navOverlay = document.querySelector('.nav_overlay');
-const textarea = document.querySelector('textarea')
+const mainContainer = document.querySelector('.main_container');
+const navItem = document.querySelector('.nav_item');
 
 hamburgerIcon.addEventListener('click', () => {
   hamburgerIcon.classList.toggle('nav_hamburger--open');
 
   navOverlay.classList.toggle('nav_overlay--show');
-})
+});
 
-textarea.addEventListener("keyup", e => {
-  textarea.style.height = '300px';
-  let scHeight = e.target.scrollHeight;
-  textarea.style.height = `${scHeight}px`
-})
+navOverlay.addEventListener('click', event => {
+  if (navOverlay.contains(event.target)) {
+    hamburgerIcon.classList.toggle('nav_hamburger--open');
+
+    navOverlay.classList.toggle('nav_overlay--show');
+  }
+});
