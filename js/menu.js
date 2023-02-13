3689614af093f09e8bf406fb958515833a8b8250
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-13 17:17:53 -0500

    agregue una funcion para evitar que el menu siga abierto apesar de que esten haciedo scroll

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,14 @@ const hamburgerIcon = document.querySelector('.nav_hamburger');
 const navOverlay = document.querySelector('.nav_overlay');
 const mainContainer = document.querySelector('.main_container');
 const navItem = document.querySelector('.nav_item');
+let menuAbierto = document.querySelector('.nav_overlay--show');
 
 hamburgerIcon.addEventListener('click', () => {
   hamburgerIcon.classList.toggle('nav_hamburger--open');
 
   navOverlay.classList.toggle('nav_overlay--show');
+
+  cerrarMenu();
 });
 
 navOverlay.addEventListener('click', event => {
@@ -16,3 +19,19 @@ navOverlay.addEventListener('click', event => {
     navOverlay.classList.toggle('nav_overlay--show');
   }
 });
+
+// Cierre del menu
+
+function cerrarMenu() {
+  window.addEventListener('scroll', () => {
+    if (navOverlay.classList.contains("nav_overlay--show")) {
+      closeMenu();
+    };
+  });
+  
+  function closeMenu() {
+    hamburgerIcon.classList.remove('nav_hamburger--open');
+  
+    navOverlay.classList.remove('nav_overlay--show');
+  };
+};
