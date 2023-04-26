8e2e750df2dcfac0989321522383ad73ce40ff70
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-04-26 10:48:44 -0500

    agregue mi efecto parallax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 const submit = document.querySelector('#form');
+
+window.onscroll = function () {
+    let position = window.pageYOffset || document.documentElement.scrollTop;
+
+    const fotoHero = document.querySelector('.hero--foto');
+
+    fotoHero.style.bottom = position * 0.1 + 'px';
+}
